866a7a5e46dc5f36e0b15a1450102823f164ae50
anirudh <anirudh04mishra@gmail.com>
2023-02-09 15:44:27 +0530

    fix: surge

--- src/App.jsx
+++ src/App.jsx
@@ -31,21 +31,22 @@ function App () {
   return (
     <div className="App">
       {/* <img src={me} /> */}
-      <div className="line-drawing-demo h-screen w-screen flex flex-col items-center justify-center gap-3">
+      <span style={{ "opacity": 0.01, "userSelect": "none" }} className="absolute this-is-for-google-ligthouse-lcp">T</span>
+      <div className="line-drawing-demo min-h-screen min-w-screen flex flex-col items-center justify-center gap-3">
         <Me className="md:block hidden px-6" />
         <MeFirst className="md:hidden sm:w-auto xs:w-[16rem] w-[13rem] flex mx-6" />
         <MeLast className="md:hidden sm:w-auto xs:w-[13rem] w-[11rem] flex mx-6" />
         <div className="sm:mt-8 mt-5 sm:text-2xl text-3xl flex flex-row justify-center md:gap-4 gap-6">
-          <a target={"_blank"} referrerPolicy="no-referrer" href={"https://github.com/anirudhgray"}>
+          <a aria-label='GitHub' target={"_blank"} referrerPolicy="no-referrer" href={"https://github.com/anirudhgray"}>
             <AiOutlineGithub />
           </a>
-          <a target={"_blank"} referrerPolicy="no-referrer" href={"https://www.linkedin.com/in/anirudh-mishra/"}>
+          <a aria-label='LinkedIn' target={"_blank"} referrerPolicy="no-referrer" href={"https://www.linkedin.com/in/anirudh-mishra/"}>
             <AiOutlineLinkedin />
           </a>
-          <a target={"_blank"} referrerPolicy="no-referrer" href={" mailto:anirudh04mishra@gmail.com"}>
+          <a aria-label='email' target={"_blank"} referrerPolicy="no-referrer" href={" mailto:anirudh04mishra@gmail.com"}>
             <AiOutlineMail />
           </a>
-          <a target={"_blank"} referrerPolicy="no-referrer" href={"https://drive.google.com/file/d/1fpS8gIELzxlp4ncQnO6V-_9z8VGuIZis/view?usp=sharing"}>
+          <a aria-label='resume' target={"_blank"} referrerPolicy="no-referrer" href={"https://drive.google.com/file/d/1fpS8gIELzxlp4ncQnO6V-_9z8VGuIZis/view?usp=sharing"}>
             <AiFillFileText />
           </a>
         </div>
